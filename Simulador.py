476872aaa1bf103bf9956c736375ddf5196636a0
armandobecerril <61662465+armandobecerril@users.noreply.github.com>
2023-10-12 11:52:46 -0600

    Add files via upload

---
+++ Simulador.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, jsonify, request, redirect, url_for, send_from_directory
+from flask_socketio import SocketIO, send, emit
+import time
+import os
+#################################################
+import json
+from time import time as t
+import numpy as np
+import math
+
+app = Flask(__name__,static_url_path="/video_index/static")
+socketio = SocketIO(app)
+
+@app.route('/video_index/uploader', methods = ['GET', 'POST'])
+def upload():
+	global filename
+	global datos
+	if request.method == 'GET':
+		filename = request.args.get('file')
+		if filename == 'CCTV.mp4':
+			datos = np.load('/Users/armandobecerril/Downloads/LASTSuspiciousActivityRecognition/simulador_Video_Analysis/cctv_values.npy')
+		elif filename == 'Fast_Food.mp4':
+			datos = np.load('/Users/armandobecerril/Downloads/LASTSuspiciousActivityRecognition/simulador_Video_Analysis/fast_values.npy')
+		elif filename == 'Robo.mp4':
+			datos = np.load('/Users/armandobecerril/Downloads/LASTSuspiciousActivityRecognition/simulador_Video_Analysis/robo_values.npy')
+		elif filename == 'Crimes.mp4':
+			datos = np.load('/Users/armandobecerril/Downloads/LASTSuspiciousActivityRecognition/simulador_Video_Analysis/crimes_values.npy')
+		elif filename == 'Supermercado.mp4':
+			datos = np.load('/Users/armandobecerril/Downloads/LASTSuspiciousActivityRecognition/simulador_Video_Analysis/super_values.npy')
+		elif filename == 'Asalto.mp4':
+			datos = np.load('/Users/armandobecerril/Downloads/LASTSuspiciousActivityRecognition/simulador_Video_Analysis/asalto_values.npy')
+		print ("datos: ", datos.shape)
+		print ("nombre del video en get: ", filename)
+		return render_template('./index.html',nameVideo=filename)
+
+@socketio.on('my event')
+def handle_my_custome_event( json_data ):
+	print('Received something: ' + str(json_data))
+	n = json_data.get('i')
+	s = json_data.get('state')
+	print('state: ', s)
+	t_state = math.floor(int(s))
+	print (" index ", t_state)
+	clase = datos[t_state][0]
+	accuracy = datos[t_state][1]
+	time.sleep(0.4)
+	respond = {"results" : {"class_name": clase, "score": accuracy, "limit": 1000, "status": 1} }
+	result = json.dumps(respond)
+	print ('Enviando.............')
+	socketio.emit('my response', result)
+
+
+@app.route('/video_index/')
+def index():
+    return render_template('./base.html', nameVideo=None)    
+
+if __name__ == '__main__':
+	socketio.run(app, debug = True, port=5007, allow_unsafe_werkzeug=True)
